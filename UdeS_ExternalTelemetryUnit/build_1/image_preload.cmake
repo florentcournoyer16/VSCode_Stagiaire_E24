42da9dc8a5c0c2bfe05931f8e50e8b8e408e758d
TaupePoilue <charles.normandeau1340@gmail.com>
2024-08-09 14:33:12 -0400

    INITIAL COMMIT 23

---
+++ UdeS_ExternalTelemetryUnit/build_1/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840_customboard" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/norc2802/5_usens/ci/vscode_stagiaire_e24/nrf52840_customboard;c:/norc2802/5_usens/ci/vscode_stagiaire_e24;C:/ncs/v2.5.0/nrf;C:/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/norc2802/5_uSens/CI/VSCode_Stagiaire_E24/UdeS_ExternalTelemetryUnit/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
