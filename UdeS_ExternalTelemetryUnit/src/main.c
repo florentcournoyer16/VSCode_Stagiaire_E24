99f511e1e8914795553bd0da1e6a8cfe447c62c2
Florent <florentcournoyer@proton.me>
2024-08-13 17:18:37 -0400

    added k_malloc

--- UdeS_ExternalTelemetryUnit/src/main.c
+++ UdeS_ExternalTelemetryUnit/src/main.c
@@ -7,29 +7,30 @@
 /** @file
  *  @brief Peripheral Heart Rate over LE Coded PHY sample
  */
+#include "cortical_implant_node.h"
+#include <dk_buttons_and_leds.h>
+#include <errno.h>
+#include <nrfx_log.h>
 #include <stddef.h>
 #include <string.h>
-#include <errno.h>
-#include <zephyr/kernel.h>
-#include <zephyr/types.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/drivers/i2c.h>
-#include <zephyr/drivers/gpio.h>
 #include <zephyr/device.h>
-#include <dk_buttons_and_leds.h>
-#include <nrfx_log.h>
-#include "cortical_implant_node.h"
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/types.h>
 // #include "pairing_basic_node.h"
 // #include "pairing_basic_coord.h"
-#include "usb_console.h"
 #include "hw_cfg.h"
+#include "usb_console.h"
 /* Private function prototype ************************************************/
 
 int main(void)
 {
 	uint32_t err;
 	uint32_t led_status = 0;
-	if (!gpio_is_ready_dt(&led0)&!gpio_is_ready_dt(&led1)&!gpio_is_ready_dt(&led2)&!gpio_is_ready_dt(&led3)&!gpio_is_ready_dt(&led4)&!gpio_is_ready_dt(&uwb_irq_pin)) {
+	if (!gpio_is_ready_dt(&led0) & !gpio_is_ready_dt(&led1) & !gpio_is_ready_dt(&led2) & !gpio_is_ready_dt(&led3) & !gpio_is_ready_dt(&led4) & !gpio_is_ready_dt(&uwb_irq_pin))
+	{
 		return 0;
 	}
 
@@ -39,21 +40,24 @@ int main(void)
 	gpio_pin_configure_dt(&led3, GPIO_OUTPUT_INACTIVE);
 	gpio_pin_configure_dt(&led4, GPIO_OUTPUT_INACTIVE);
 
-    err = enable_usb_console();
-    if (err){
-        printk("USB enabling failed (err %d)\n", err);
-        return 0;
-    }
+	err = enable_usb_console();
+	if (err)
+	{
+		printk("USB enabling failed (err %d)\n", err);
+		return 0;
+	}
 
 	err = dk_leds_init();
-	if (err) {
+	if (err)
+	{
 		printk("LEDs init failed (err %d)\n", err);
 		return 0;
 	}
 
 	cortical_implant_routine();
-	
-	for (;;) {
+
+	for (;;)
+	{
 		dk_set_led(RUN_STATUS_LED, (++led_status) % 2);
 		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
 	}
